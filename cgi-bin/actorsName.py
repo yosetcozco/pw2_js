742200cdc6040f1cb0c324672dd94019bd2a9007
yosetCozco <yodetcozco@gmail.com>
2020-06-13 11:58:16 -0500

    imdb.db moved to folder sqlite and line of conection modified

---
+++ cgi-bin/actorsName.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#WARNING!
+#Do not forget check your own path for python3!
+import json
+import cgi
+import sqlite3
+
+form = cgi.FieldStorage() 
+name = form.getvalue('name')
+name = 'Ford' #only for testing
+
+conn = sqlite3.connect('../sqlite/imdb.db')
+cur = conn.cursor()
+sql = "SELECT * FROM Actor WHERE Name like '%" + name + "%'"
+#print(sql)
+data = []
+for row in cur.execute(sql):
+  data.append({
+    'id': row[0],
+    'name': row[1]
+    })
+conn.close()
+print("Content-Type: application/json\n\n")
+print(json.dumps(data))
